7e02737615c19e5bdfec7744e75e40ce986e1133
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-05-20 23:26:02 +0800

    Add test

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts", "!**/node_modules/**", "!**/vendor/**"],
 }
---
+++ test/base.spec.ts
@@ -0,0 +1,71 @@
+import store from "../src/index"
+
+describe("local-store-pro api test", () => {
+  test("test setNamespace getNamespace", () => {
+    const namespace = "my-store"
+    ;(store as any).setNamespace(namespace)
+    expect((store as any).getNamespace()).toBe(namespace)
+  })
+  test("test setStore getStore", () => {
+    ;(store as any).setStore(localStorage)
+    expect((store as any).getStore).toBeUndefined()
+  })
+  test("test set get remove", () => {
+    const key = "token"
+    const key2 = "token2"
+    store.set(key, 1)
+    expect(store.get(key)).toBe(1)
+    store.set(key2, 2)
+    expect(store.get(key2)).toBe(2)
+    store.set(key, "1")
+    expect(store.get(key)).toBe("1")
+    store.remove(key2)
+    expect(store.get(key2)).toBe(null)
+  })
+  test("test set null undefined", () => {
+    store("theme", null)
+    expect(store("theme")).toBe(null)
+    store("theme", "dark")
+    expect(store("theme")).toBe("dark")
+    store("theme", undefined) // remove theme
+    expect(store("theme")).toBe(null)
+  })
+  test("test set get from expires", async () => {
+    store.set("food", 3, 3)
+    expect(store.get("food")).toBe(3)
+    const food = await new Promise(resolve =>
+      setTimeout(function () {
+        resolve(store.get("food"))
+      }, 3001),
+    )
+    expect(food).toBe(null)
+  }, 7000)
+  test("test set subscribe", done => {
+    store.set("token", 3, 3)
+    store.subscribe("token", ev => {
+      expect(ev.newValue).toBe(5)
+      expect(ev.oldValue).toBe(null)
+      done()
+    })
+    setTimeout(() => {
+      store.set("token", 5, 3)
+    }, 3001)
+  }, 7000)
+  test("test has", () => {
+    store("color", "#fff")
+    expect(store.has("color")).toBeTruthy()
+    expect(store.has("color2")).toBeFalsy()
+  })
+  test("test subscribe unsubscribe", () => {
+    store.subscribe("key1", () => {})
+    store.subscribe("key2", () => {})
+    store.subscribe("key3", () => {})
+    store.unsubscribe(["key1", "key2"])
+  })
+  test("test Exception", () => {
+    const consoleWarnSpy = jest.spyOn(console, "warn")
+    store.set(1 as any, 1)
+    expect(store(1 as any)).toBe(null)
+    expect(consoleWarnSpy).toHaveBeenCalledWith("store failed, entry a valid string key.")
+  })
+})
